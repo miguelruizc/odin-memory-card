05132a5c2578d4d600a6ba39e706fd7953940a5d
miguelruizc <buumchakalaka@gmail.com>
2024-06-17 20:54:06 +0200

    Add conditional renders for cards (loading, error, good)

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,25 +1,53 @@
 import { useEffect, useState } from 'react';
 
 const Card = () => {
-	const [name, setName] = useState('');
-	const [sprite, setSprite] = useState('');
+	const [name, setName] = useState(null);
+	const [sprite, setSprite] = useState(null);
+	const [isPending, setIsPending] = useState(true);
+	const [error, setError] = useState(null);
 
 	useEffect(() => {
 		const randomPokemon = Math.floor(Math.random() * 386) + 1;
 
 		fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemon}`)
-			.then((response) => response.json())
-			.then((data) => {
-				setName(data.name);
-				setSprite(data.sprites.front_default);
+			.then((response) => {
+				if (!response.ok) {
+					console.log(response.status);
+					throw new Error('HTTP error!');
+				}
+				response.json().then((data) => {
+					setName(data.name);
+					setSprite(data.sprites.front_default);
+					setIsPending(false);
+				});
 			})
-			.catch((error) => console.error('Error fetching data', error));
+			.catch((err) => {
+				setError(err);
+				setIsPending(false);
+				console.error('Error fetching data', err);
+			});
 	}, []);
 
 	return (
 		<div className="card">
-			<h2>{name}</h2>
-			<img src={sprite}></img>
+			{isPending && (
+				<>
+					<h2>Loading...</h2>
+					<img src="./src/assets/pokeball.png"></img>
+				</>
+			)}
+			{error && (
+				<>
+					<h2>{error}</h2>
+					<img src="./src/assets/pokeball.png"></img>
+				</>
+			)}
+			{name && (
+				<>
+					<h2>{name}</h2>
+					<img src={sprite}></img>
+				</>
+			)}
 		</div>
 	);
 };
