fcee2d6cdcadc04f492329577f3c64d9962953ad
miguelruizc <buumchakalaka@gmail.com>
2024-06-17 20:11:26 +0200

    Add pikachu card generated from pokeAPI.co

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,13 +1,23 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const Card = () => {
-	const [name, setName] = useState('Pikachu');
-	const [image, setImage] = useState('./src/assets/pikachu.png');
+	const [name, setName] = useState('');
+	const [sprite, setSprite] = useState('');
+
+	useEffect(() => {
+		fetch('https://pokeapi.co/api/v2/pokemon/25')
+			.then((response) => response.json())
+			.then((data) => {
+				setName(data.name);
+				setSprite(data.sprites.front_default);
+			})
+			.catch((error) => console.error('Error fetching data', error));
+	}, []);
 
 	return (
 		<div className="card">
 			<h2>{name}</h2>
-			<img src={image}></img>
+			<img src={sprite}></img>
 		</div>
 	);
 };
