0c9a7b83e86d542bf397799eb8aea1660b071b3a
miguelruizc <buumchakalaka@gmail.com>
2024-06-18 12:10:13 +0200

    Add game data variable to App state

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
+import { useState } from 'react';
 import Card from './Card.jsx';
 
+const MIN_CARDS = 2;
+const MAX_CARDS = 99;
+
 function App() {
+	const [highestScore, setHighestScore] = useState(0);
+	const [currentScore, setCurrentScore] = useState(0);
+	const [scoreMultiplier, setScoreMultiplier] = useState(0);
+	const [isPlaying, setIsPlaying] = useState(false);
+	const [isComplete, setIsComplete] = useState(false);
+	const [numCards, setNumCards] = useState(MIN_CARDS);
+	const [cardsArray, setCardsArray] = useState([]);
+
 	return (
 		<>
 			<div className="title">
 				<h1>Memory Cards</h1>
 			</div>
 			<div className="scores">
-				<p>Highest score: {100}</p>
-				<p>Current score: {99}</p>
+				<p>Highest score: {highestScore}</p>
+				<p>Current score: {currentScore}</p>
 			</div>
 			<div className="game-info">
 				<button>Play</button>
